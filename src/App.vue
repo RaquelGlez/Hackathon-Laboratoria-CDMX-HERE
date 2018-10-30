116edfaf1e990fdf12747b863017cbb6bb85c1c9
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-10-30 00:42:53 -0600

    Agrega mapa con geolozalizacion de un punto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,48 @@
 <template>
   <div id="app">
     <router-view/>
+    <!-- <HereMap1
+      appId = "kjUJtSCkxIEv0qQOYCUE"
+      appCode = "3lVlTVLLiPXEesKAAu1HgQ"
+      [start]="start"
+      [finish]="finish"
+      lat= "19.4978"
+      lng= "-99.1269"
+      width= "30%"
+      height= "500px" /> -->
+      <HereMap2
+      appId = "kjUJtSCkxIEv0qQOYCUE"
+      appCode = "3lVlTVLLiPXEesKAAu1HgQ"
+      location= " Benito Juarez, CDMX"
+      lat= "19.3983994"
+      lng= "-99.1576614"
+      width= "30%"
+      height= "500px" />
   </div>
 </template>
+
+<script>
+  //import HereMap1 from "./components/HereMap1.vue"
+  import HereMap2 from "./components/HereMap2.vue"
+  
+  export default {
+    name: "app",
+    components: {
+      //HereMap1
+      HereMap2
+    }
+  }
+
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+</style>
+
 
---
+++ src/components/HereMap2.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="here-map"></div>
+        <div ref="map" v-bind:style="{width: width, height: height}"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HereMap2",
+        data() { 
+            return {
+                map: { },
+                platform: { },
+                geocoder:{ }
+            }
+        },
+        props: { 
+            appId: String,
+            appCode: String,
+            lat: String,
+            lng: String,
+            width: String,
+            height: String,
+            location: String
+        },
+        created () {
+            this.platform = new H.service.Platform ({
+                "app_id": this.appId,
+                "app_code": this.appCode
+            });
+            this.geocoder = this.platform.getGeocodingService();
+         },
+
+        mounted () { 
+            this.map = new H.Map (
+                this.$refs.map,
+                this.platform.createDefaultLayers().normal.map,
+                {
+                    zoom: 14,
+                    center: { lng: this.lng, lat: this.lat}
+                }
+            );
+            this.geocoder.geocode({ searchText: this.location }, data => {
+                if(data.Response.View.length > 0) {
+                    if(data.Response.View[0].Result.length > 0) {
+                        var coords = data.Response.View[0].Result[0].Location.DisplayPosition;
+                        this.map.setCenter({lat: coords.Latitude, lng: coords.Longitude });
+                        this.map.addObject(new H.map.Marker({lat: coords.Latitude, lng: coords.Longitude}));
+                    }
+                }
+            }, error => {
+                console.error(error);
+            });
+            
+        }
+    }
+</script>
+
+<style scoped></style>
