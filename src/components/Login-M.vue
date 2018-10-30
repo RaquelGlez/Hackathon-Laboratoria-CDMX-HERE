9f24ef6683c96099ad10bd045d9d3acde175b8db
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-10-29 22:31:32 -0600

    y

--- src/components/Login-M.vue
+++ src/components/Login-M.vue
@@ -5,13 +5,34 @@
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" v-model="username">
     <label for="exampleInputEmail1">Ingresa tu email</label>
     <input type="email" class="form-control" v-model="email"  placeholder="Email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <small id="emailHelp" class="form-text text-muted">Tu contraseña e email son intrasferibles</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Ingresa tu password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" v-model = "password" placeholder="Password">
+    <input type="password" class="form-control" v-model ="password" placeholder="Password">
   </div>
   <button type="submit" class="px-5 btn btn-warning">Submit</button>
 </form>
+</template>
+
+<script>
+export default{
+name: "Sing In",
+data(){
+  return{
+    username:"",
+    email:"",
+    password:""
+  };
+},
+methods:{
+  handleSignInUser(){
+    this.$store.dispatch("signinUser", {
+      
+    })
+  }
+}
+}
+</script>  
    
 </template>
